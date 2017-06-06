3d499a0c30f0af50b86aedb1a53164282e7eaa35
padraic <padraic.brady@gmail.com>
2017-06-06 19:23:57 +0000

    Apply fixes from StyleCI

--- src/Handler/HandleAddPrefix.php
+++ src/Handler/HandleAddPrefix.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 /*
- * This file is part of the webmozart/php-scoper package.
+ * This file is part of the humbug/php-scoper package.
  *
- * (c) Bernhard Schussek <bschussek@gmail.com>
+ * Copyright (c) 2017 Théo FIDRY <theo.fidry@gmail.com>,
+ *                    Pádraic Brady <padraic.brady@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- src/Throwable/Exception/ParsingException.php
+++ src/Throwable/Exception/ParsingException.php
@@ -1,5 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the humbug/php-scoper package.
+ *
+ * Copyright (c) 2017 Théo FIDRY <theo.fidry@gmail.com>,
+ *                    Pádraic Brady <padraic.brady@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Humbug\PhpScoper\Throwable\Exception;
 
 final class ParsingException extends RuntimeException
