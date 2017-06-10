0124038eefbb9f8d0b9741594e51cce8cb1df09d
Théo FIDRY <theo.fidry@gmail.com>
2017-06-10 17:45:28 +0100

    Fix tests

--- src/Console/Command/AddPrefixCommand.php
+++ src/Console/Command/AddPrefixCommand.php
@@ -20,6 +20,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
