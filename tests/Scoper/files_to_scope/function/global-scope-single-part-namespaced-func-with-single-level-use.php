afa5b01b2327aa9dd164348e1b4815a7e0774384
Théo FIDRY <theo.fidry@gmail.com>
2017-08-27 14:35:53 +0100

    Add function calls

---
+++ tests/Scoper/files_to_scope/function/global-scope-single-part-namespaced-func-with-single-level-use.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+// Fixture file used to compare what the expected result is for the scoping for a given input
+
+return [
+    'meta' => [
+        'title' => 'single-part namespaced function call in the global scope with a single-level use statement',
+        // Default values. If not specified will be the one used
+        'prefix' => 'Humbug',
+        'whitelist' => [],
+    ],
+
+    // See tests for the use statements as to why we don't touch the use statement.
+    // The use statement cannot be prefixed, but as the class is in two-parts this
+    // is enough to know we can prefix it.
+    'two-parts' =>  <<<'PHP'
+<?php
+
+use Foo;
+
+Foo\main();
+----
+<?php
+
+use Foo;
+
+\Humbug\Foo\main();
+
+PHP
+    ,
+
+    // See tests for the use statements as to why we don't touch the use statement.
+    // As there is nothing in PHP core with more than two-parts, we can safely prefix.
+    'FQ two-parts' =>  <<<'PHP'
+<?php
+
+use Foo;
+
+\Foo\main();
+----
+<?php
+
+use Foo;
+
+\Humbug\Foo\main();
+
+PHP
+    ,
+
+    // See tests for the use statements as to why we don't touch the use statement.
+    // Whitelisting a function call has no effect
+    'whitelisted two-parts' =>  [
+        'whitelist' => ['Foo\main'],
+        'payload' => <<<'PHP'
+<?php
+
+use Foo;
+
+Foo\main();
+----
+<?php
+
+use Foo;
+
+\Humbug\Foo\main();
+
+PHP
+    ],
+
+    // See tests for the use statements as to why we don't touch the use statement.
+    'FQ whitelisted two-parts' =>  [
+        'whitelist' => ['Foo\main'],
+        'payload' => <<<'PHP'
+<?php
+
+use Foo;
+
+\Foo\main();
+----
+<?php
+
+use Foo;
+
+\Humbug\Foo\main();
+
+PHP
+    ],
+];
