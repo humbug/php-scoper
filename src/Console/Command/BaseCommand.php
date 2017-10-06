dba8ce926ff9fbab442bf01723adce01e84bed21
Théo FIDRY <t.fidry@cwd.at>
2017-10-06 12:34:58 +0200

    Fix CS

--- src/Console/Command/BaseCommand.php
+++ src/Console/Command/BaseCommand.php
@@ -14,20 +14,11 @@ declare(strict_types=1);
 
 namespace Humbug\PhpScoper\Console\Command;
 
-use Humbug\PhpScoper\Console\Configuration;
-use Humbug\PhpScoper\Handler\HandleAddPrefix;
-use Humbug\PhpScoper\Logger\ConsoleLogger;
 use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\RuntimeException;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\OutputStyle;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Filesystem\Filesystem;
-use Throwable;
 
 abstract class BaseCommand extends Command
 {
--- src/Console/Command/InitCommand.php
+++ src/Console/Command/InitCommand.php
@@ -14,10 +14,6 @@ declare(strict_types=1);
 
 namespace Humbug\PhpScoper\Console\Command;
 
-use Humbug\PhpScoper\Logger\UpdateConsoleLogger;
-use Humbug\SelfUpdate\Updater;
-use PHAR;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\FormatterHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -25,7 +21,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\OutputStyle;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
-use Throwable;
 
 final class InitCommand extends BaseCommand
 {
