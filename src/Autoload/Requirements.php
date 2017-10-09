e93a4a050a00c7f41d8714e2633dd6d71786746b
Théo FIDRY <theo.fidry@gmail.com>
2017-10-09 22:34:33 +0200

    Check requirements when running from a PHAR (#116)
    An issue one may come across when running a PHAR, as opposed to when you install a tool via Composer, is that no check whatsoever is run.
    
    By leveraging [symfony/requirements-checker](https://github.com/symfony/requirements-checker/), a check is now run when running the PHAR. The check is run _before anything_. The output is the following when checks are passing in very verbose mode (`-vv` or `-vvv`):
    
    ![screen shot 2017-10-07 at 12 30 23](https://user-images.githubusercontent.com/5175937/31354701-3cc2cb9c-ad2f-11e7-8787-92c8c9a96b5e.png)
    
    If the very verbose mode is not given, nothing is displayed unless an error occurs. If the checks don't pass, regardless of the verbosity specified, the errors will be printed:
    
    ![screen shot 2017-10-07 at 12 28 00](https://user-images.githubusercontent.com/5175937/31354704-3ff9b65e-ad2f-11e7-906c-4dce9104113f.png)

---
+++ src/Autoload/Requirements.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file is part of the humbug/php-scoper package.
+ *
+ * Copyright (c) 2017 Théo FIDRY <theo.fidry@gmail.com>,
+ *                    Pádraic Brady <padraic.brady@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Humbug\PhpScoper\Autoload;
+
+use Composer\Semver\Semver;
+use Symfony\Requirements\RequirementCollection;
+
+/**
+ * Collect the list of requirements for running the project. Code in this file must be PHP 5.3+ compatible as is used
+ * to know if PHP-Scoper can be run or not.
+ */
+final class Requirements extends RequirementCollection
+{
+    public function __construct($composerJson)
+    {
+        $composerConfig = $this->readComposer($composerJson);
+
+        $this->addPhpVersionRequirement($composerConfig);
+        $this->addExtensionRequirements($composerConfig);
+    }
+
+    private function readComposer($composerJson)
+    {
+        $composer = json_decode(file_get_contents($composerJson), true);
+
+        return $composer['require'];
+    }
+
+    private function addPhpVersionRequirement(array $composerConfig)
+    {
+        $installedPhpVersion = phpversion();
+        $requiredPhpVersion = $composerConfig['php'];
+
+        $this->addRequirement(
+            Semver::satisfies(phpversion(), $requiredPhpVersion),
+            sprintf(
+                'PHP version must satisfies "%s" ("%s" installed)',
+                $requiredPhpVersion,
+                $installedPhpVersion
+            ),
+            ''
+        );
+    }
+
+    private function addExtensionRequirements(array $composerConfig)
+    {
+        foreach ($composerConfig as $package => $constraint) {
+            if (preg_match('/^ext-(?<extension>.+)$/', $package, $matches)) {
+                $this->addExtensionRequirement($matches['extension']);
+            }
+        }
+    }
+
+    /**
+     * @param string $extension Extension name, e.g. `iconv`.
+     */
+    private function addExtensionRequirement($extension)
+    {
+        $this->addRequirement(
+            extension_loaded($extension),
+            sprintf(
+                'The extension "%s" must be enabled.',
+                $extension
+            ),
+            ''
+        );
+    }
+}
---
+++ tests/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the humbug/php-scoper package.
+ *
+ * Copyright (c) 2017 Théo FIDRY <theo.fidry@gmail.com>,
+ *                    Pádraic Brady <padraic.brady@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../src/functions.php';
+require __DIR__.'/functions.php';
