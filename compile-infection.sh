6dee6a602efa21df5444d84952d27dd1300cfda7
Théo FIDRY <theo.fidry@gmail.com>
2018-02-22 23:44:04 +0000

    Cleanup

--- compile-infection.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-bin/php-scoper add-prefix vendor/infection/infection -obuild/infection -f
-composer dump-autoload -dbuild/infection
-
-php build/infection/vendor/infection/infection/bin/infection
-
-# config file for INFECTION_COMPOSER_INSTALL
