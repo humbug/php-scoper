5c586fb3234a9c52f94811119b11ea6f93daf3be
Tomas Votruba <tomas.vot@gmail.com>
2018-05-12 16:37:35 +0200

    update WhitelistedClassAppender

--- src/PhpParser/NodeVisitor/WhitelistedClassAppender.php
+++ src/PhpParser/NodeVisitor/WhitelistedClassAppender.php
@@ -22,6 +22,7 @@ use PhpParser\Node\Name;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Interface_;
 use PhpParser\Node\Stmt\Namespace_;
 use PhpParser\NodeVisitorAbstract;
@@ -89,21 +90,21 @@ final class WhitelistedClassAppender extends NodeVisitorAbstract
             }
 
             /** @var Class_ $stmt */
-            $name = FullyQualified::concat($namespace->name, $stmt->name);
+            $name = FullyQualified::concat((string) $namespace->name, (string) $stmt->name);
             $originalName = $name->slice(1);
 
             if (false === in_array((string) $originalName, $this->whitelist, true)) {
                 continue;
             }
 
-            $newStmts[] = new FuncCall(
+            $newStmts[] = new Expression(new FuncCall(
                 new Name('class_alias'),
                 [
                     new Arg(
-                        new String_($name)
+                        new String_((string) $name)
                     ),
                     new Arg(
-                        new String_($originalName)
+                        new String_((string) $originalName)
                     ),
                     new Arg(
                         new ConstFetch(
@@ -112,7 +113,7 @@ final class WhitelistedClassAppender extends NodeVisitorAbstract
                     ),
                 ],
                 ['whitelist_class_alias' => true]
-            );
+            ));
         }
 
         $namespace->stmts = $newStmts;
