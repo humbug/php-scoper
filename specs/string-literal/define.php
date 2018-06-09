38b71552cf0886952d4f8a9da9acc56af8f06504
Théo FIDRY <theo.fidry@gmail.com>
2018-06-09 14:17:49 +0100

    Ensure string literals in define statements are properly prefixed (#221)

---
+++ specs/string-literal/define.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the humbug/php-scoper package.
+ *
+ * Copyright (c) 2017 Théo FIDRY <theo.fidry@gmail.com>,
+ *                    Pádraic Brady <padraic.brady@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'meta' => [
+        'title' => 'String literal assigned as a constant declared with `define()`',
+        // Default values. If not specified will be the one used
+        'prefix' => 'Humbug',
+        'whitelist' => [],
+        'whitelist-global-constants' => true,
+    ],
+
+    'FQCN string argument: transform into a FQCN and prefix it' => <<<'PHP'
+<?php
+
+define('X', 'Symfony\\Component\\Yaml\\Yaml');
+define('X', '\\Symfony\\Component\\Yaml\\Yaml');
+define('X', 'Humbug\\Symfony\\Component\\Yaml\\Yaml');
+define('X', '\\Humbug\\Symfony\\Component\\Yaml\\Yaml');
+
+----
+<?php
+
+namespace Humbug;
+
+\define('X', 'Humbug\\Symfony\\Component\\Yaml\\Yaml');
+\define('X', 'Humbug\\Symfony\\Component\\Yaml\\Yaml');
+\define('X', 'Humbug\\Symfony\\Component\\Yaml\\Yaml');
+\define('X', 'Humbug\\Symfony\\Component\\Yaml\\Yaml');
+
+PHP
+    ,
+];
