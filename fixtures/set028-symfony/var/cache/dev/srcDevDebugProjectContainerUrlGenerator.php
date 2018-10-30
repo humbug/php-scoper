c27796e46a66eee222980133da58ef65e1e68919
Théo FIDRY <theo.fidry@gmail.com>
2018-10-30 01:13:35 +0100

    Update nikic tests

---
+++ fixtures/set028-symfony/var/cache/dev/srcDevDebugProjectContainerUrlGenerator.php
@@ -0,0 +1,44 @@
+<?php
+
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Psr\Log\LoggerInterface;
+
+/**
+ * This class has been auto-generated
+ * by the Symfony Routing Component.
+ */
+class srcDevDebugProjectContainerUrlGenerator extends Symfony\Component\Routing\Generator\UrlGenerator
+{
+    private static $declaredRoutes;
+    private $defaultLocale;
+
+    public function __construct(RequestContext $context, LoggerInterface $logger = null, string $defaultLocale = null)
+    {
+        $this->context = $context;
+        $this->logger = $logger;
+        $this->defaultLocale = $defaultLocale;
+        if (null === self::$declaredRoutes) {
+            self::$declaredRoutes = array(
+    );
+        }
+    }
+
+    public function generate($name, $parameters = array(), $referenceType = self::ABSOLUTE_PATH)
+    {
+        $locale = $parameters['_locale']
+            ?? $this->context->getParameter('_locale')
+            ?: $this->defaultLocale;
+
+        if (null !== $locale && (self::$declaredRoutes[$name.'.'.$locale][1]['_canonical_route'] ?? null) === $name) {
+            unset($parameters['_locale']);
+            $name .= '.'.$locale;
+        } elseif (!isset(self::$declaredRoutes[$name])) {
+            throw new RouteNotFoundException(sprintf('Unable to generate a URL for the named route "%s" as such route does not exist.', $name));
+        }
+
+        list($variables, $defaults, $requirements, $tokens, $hostTokens, $requiredSchemes) = self::$declaredRoutes[$name];
+
+        return $this->doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $referenceType, $hostTokens, $requiredSchemes);
+    }
+}
