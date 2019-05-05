10c871b7a34bbbecdd2db377dc0aa3f6385ed540
Théo FIDRY <theo.fidry@gmail.com>
2019-05-05 12:17:56 +0200

    Fix CS

--- bin/check-composer-root-version.php
+++ bin/check-composer-root-version.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the humbug/php-scoper package.
+ *
+ * Copyright (c) 2017 Théo FIDRY <theo.fidry@gmail.com>,
+ *                    Pádraic Brady <padraic.brady@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require_once __DIR__.'/root-version.php';
 
 $composerRootVersion = get_composer_root_version(get_last_tag_name());
