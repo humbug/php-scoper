2f26cda2f1eb9ed491960e827d32d8360350b751
Théo FIDRY <theo.fidry@gmail.com>
2019-06-08 21:01:46 +0200

    Update the Composer root version

--- bin/check-composer-root-version.php
+++ bin/check-composer-root-version.php
@@ -17,7 +17,7 @@ require_once __DIR__.'/root-version.php';
 try {
     $composerRootVersion = get_composer_root_version(get_last_tag_name());
 } catch (RuntimeException $exception) {
-    if ('humbug/box' !== getenv('TRAVIS_REPO_SLUG')) {
+    if (false !== getenv('TRAVIS') && false === getenv('GITHUB_TOKEN')) {
         // Ignore this PR to avoid too many builds to fail untimely or locally due to API rate limits because the last
         // release version could not be retrieved.
         return;
