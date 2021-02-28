91b6f7cc840d73bc600d4aac79c7f35eceadb5a2
Marek Dědič <developer@dedic.eu>
2021-02-28 19:31:35 +0100

    Not simplifying Isolated\Symfony\Component\Finder\Finder

--- src/PhpParser/NodeVisitor/NameStmtPrefixer.php
+++ src/PhpParser/NodeVisitor/NameStmtPrefixer.php
@@ -162,7 +162,8 @@ final class NameStmtPrefixer extends NodeVisitorAbstract
             self::array_starts_with($resolvedName->parts, $useStatement->parts) and
             !($parentNode instanceof ConstFetch and ($this->whitelist->isGlobalWhitelistedConstant($resolvedName->toString()) or $this->whitelist->isSymbolWhitelisted($resolvedName->toString(), true))) and
             !($useStatement->getAttribute('parent') and $useStatement->getAttribute('parent')->alias !== null and $this->whitelist->isSymbolWhitelisted($useStatement->toString())) and
-            !($name instanceof FullyQualified)
+            !($name instanceof FullyQualified) and
+            $resolvedName->parts !== ['Isolated', 'Symfony', 'Component', 'Finder', 'Finder']
         ) {
             return $name;
         }
