4440a06a91c2d5f41887d7f1dd4ad6b8286e0f03
Théo FIDRY <theo.fidry@gmail.com>
2021-07-11 19:07:58 +0200

    Add the missing tests

--- tests/TemplateFileTest.php
+++ tests/TemplateFileTest.php
@@ -5,14 +5,37 @@ declare(strict_types=1);
 namespace Humbug\PhpScoper;
 
 use PHPUnit\Framework\TestCase;
+use function Safe\file_get_contents;
+use function Safe\preg_match_all;
 
 /**
  * @coversNothing
  */
 final class TemplateFileTest extends TestCase
 {
+    private const TEMPLATE_PATH = __DIR__.'/../src/scoper.inc.php.tpl';
+
     public function test_the_template_file_contains_an_entry_for_each_configuration_key(): void
     {
-        $this->markTestSkipped('TODO');
+        $templateConfigKeys = self::retrieveKeys();
+
+        self::assertEqualsCanonicalizing(
+            ConfigurationKeys::KEYWORDS,
+            $templateConfigKeys,
+        );
+    }
+
+    /**
+     * @return list<string>
+     */
+    private static function retrieveKeys(): array
+    {
+        $template = file_get_contents(self::TEMPLATE_PATH);
+
+        if (preg_match_all('/\'(.*?)\' => .*/', $template, $matches)) {
+            return $matches[1];
+        }
+
+        return [];
     }
 }
