0fa38ace05f0360a32382024941b2fddbf54a666
Théo FIDRY <theo.fidry@gmail.com>
2021-07-18 12:14:23 +0200

    Fix scoping of closures return type (#524)

--- fixtures/set020-infection/scoper.inc.php
+++ fixtures/set020-infection/scoper.inc.php
@@ -2,4 +2,33 @@
 
 declare(strict_types=1);
 
-return [];
+use Isolated\Symfony\Component\Finder\Finder;
+
+$polyfillsBootstraps = array_map(
+    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
+    iterator_to_array(
+        Finder::create()
+            ->files()
+            ->in(__DIR__ . '/vendor/symfony/polyfill-*')
+            ->name('bootstrap.php'),
+        false,
+    ),
+);
+
+$polyfillsStubs = array_map(
+    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
+    iterator_to_array(
+        Finder::create()
+            ->files()
+            ->in(__DIR__ . '/vendor/symfony/polyfill-*/Resources/stubs')
+            ->name('*.php'),
+        false,
+    ),
+);
+
+return [
+    'files-whitelist' => [
+        ...$polyfillsBootstraps,
+        ...$polyfillsStubs,
+    ],
+];
