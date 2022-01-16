925688190fd4c60530dfe129a692855173b4b395
Théo FIDRY <theo.fidry@gmail.com>
2022-01-16 21:17:17 +0100

    Another attempt

--- vendor-hotfix/SimpleScoper.php
+++ vendor-hotfix/SimpleScoper.php
@@ -14,10 +14,19 @@ declare(strict_types=1);
 
 namespace KevinGH\Box\PhpScoper;
 
+use Exception;
 use Humbug\PhpScoper\Container as PhpScoperContainer;
+use Humbug\PhpScoper\Patcher\ComposerPatcher;
+use Humbug\PhpScoper\Patcher\SymfonyPatcher;
 use Humbug\PhpScoper\Scoper as PhpScoper;
+use Humbug\PhpScoper\Scoper\FileWhitelistScoper;
 use Humbug\PhpScoper\Whitelist;
 use Humbug\PhpScoper\Configuration as PhpScoperConfiguration;
+use Opis\Closure\SerializableClosure;
+use Serializable;
+use function array_map;
+use function count;
+use function var_dump;
 
 /**
  * @private
@@ -28,9 +37,27 @@ final class SimpleScoper implements Scoper
     private PhpScoperContainer $scoperContainer;
     private PhpScoper $scoper;
 
-    public function __construct(PhpScoperConfiguration $scoperConfig)
-    {
-        $this->scoperConfig = $scoperConfig;
+    /**
+     * @var list<string>
+     */
+    private array $whitelistedFilePaths;
+
+    public function __construct(
+        PhpScoperConfiguration $scoperConfig,
+        string ...$whitelistedFilePaths
+    ) {
+        $this->scoperConfig = new PhpScoperConfiguration(
+            $scoperConfig->getPath(),
+            $scoperConfig->getPrefix(),
+            $scoperConfig->getFilesWithContents(),
+            $scoperConfig->getWhitelistedFilesWithContents(),
+            self::createSerializablePatchers($scoperConfig->getPatchers()),
+            $scoperConfig->getWhitelist(),
+            $scoperConfig->getInternalClasses(),
+            $scoperConfig->getInternalFunctions(),
+            $scoperConfig->getInternalConstants(),
+        );
+        $this->whitelistedFilePaths = $whitelistedFilePaths;
     }
 
     /**
@@ -50,6 +77,8 @@ final class SimpleScoper implements Scoper
     public function changeWhitelist(Whitelist $whitelist): void
     {
         $previousConfig = $this->scoperConfig;
+
+        var_dump($whitelist);die;
 
         $this->scoperConfig = new PhpScoperConfiguration(
             $previousConfig->getPath(),
@@ -79,7 +108,7 @@ final class SimpleScoper implements Scoper
      */
     public function getPrefix(): string
     {
-        return $this->prefix;
+        return $this->scoperConfig->getPrefix();
     }
 
     private function getScoper(): PhpScoper
@@ -92,10 +121,40 @@ final class SimpleScoper implements Scoper
             $this->scoperContainer = new PhpScoperContainer();
         }
 
-        $this->scoper = $this->scoperContainer
+        $scoper = $this->scoperContainer
             ->getScoperFactory()
             ->createScoper($this->scoperConfig);
 
+        if (count($this->whitelistedFilePaths) !== 0) {
+            $scoper = new FileWhitelistScoper(
+                $scoper,
+                ...$this->whitelistedFilePaths,
+            );
+        }
+
+        $this->scoper = $scoper;
+
         return $this->scoper;
+    }
+
+    /**
+     * @param callable[] $patchers
+     *
+     * @retunr SerializableClosure[]
+     */
+    private static function createSerializablePatchers(array $patchers): array
+    {
+        return array_map(
+            static function (callable $patcher): SerializableClosure {
+                if ($patcher instanceof SymfonyPatcher
+                    || $patcher instanceof ComposerPatcher
+                ) {
+                    $patcher = static fn (string $filePath, string $prefix, string $contents) => $patcher($filePath, $prefix, $contents);
+                }
+
+                return new SerializableClosure($patcher);
+            },
+            $patchers,
+        );
     }
 }
