4c7309dc36b0d5b47b3fca4bdfb2c9f7066080f4
Théo FIDRY <theo.fidry@gmail.com>
2022-01-26 11:44:41 +0100

    Leverage SymbolRegistry in Reflector

---
+++ tests/EmptyReflectorTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the humbug/php-scoper package.
+ *
+ * Copyright (c) 2017 Théo FIDRY <theo.fidry@gmail.com>,
+ *                    Pádraic Brady <padraic.brady@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Humbug\PhpScoper;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Humbug\PhpScoper\Reflector
+ */
+class EmptyReflectorTest extends TestCase
+{
+    private Reflector $reflector;
+
+    protected function setUp(): void
+    {
+        $this->reflector = Reflector::createEmpty();
+    }
+
+    /**
+     * @dataProvider \Humbug\PhpScoper\PhpStormStubsReflectorTest::provideClasses
+     */
+    public function test_it_can_identify_internal_classes(string $class): void
+    {
+        $actual = $this->reflector->isClassInternal($class);
+
+        self::assertFalse($actual);
+    }
+
+    /**
+     * @dataProvider \Humbug\PhpScoper\PhpStormStubsReflectorTest::provideFunctions
+     */
+    public function test_it_can_identify_internal_functions(string $class, bool $expected): void
+    {
+        $actual = $this->reflector->isFunctionInternal($class);
+
+        self::assertFalse($actual);
+    }
+
+    /**
+     * @dataProvider \Humbug\PhpScoper\PhpStormStubsReflectorTest::provideConstants
+     */
+    public function test_it_can_identify_internal_constants(string $class, bool $expected): void
+    {
+        $actual = $this->reflector->isConstantInternal($class);
+
+        self::assertFalse($actual);
+    }
+}
