60b8620e63916c701325d48ba7c606b9f01c84f8
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2022-11-07 20:14:01 +0100

    Fix e2e 019 (#721)

--- fixtures/set019-symfony-console/main.php
+++ fixtures/set019-symfony-console/main.php
@@ -9,19 +9,23 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-require_once __DIR__ . '/vendor/autoload.php';
+require file_exists(__DIR__.'/vendor/scoper-autoload.php')
+    ? __DIR__.'/vendor/scoper-autoload.php'
+    : __DIR__.'/vendor/autoload.php';
 
 class HelloWorldCommand extends Command
 {
-    protected function configure()
+    protected function configure(): void
     {
         $this->setName('hello:world')
             ->setDescription('Outputs \'Hello World\'');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('Hello world!');
+
+        return self::SUCCESS;
     }
 }
 
