c5cb1a89770baa027f951165af83751b51504d16
Théo FIDRY <theo.fidry@gmail.com>
2022-11-08 22:09:31 +0100

    Add spec for enum support

---
+++ _specs/declaration.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the humbug/php-scoper package.
+ *
+ * Copyright (c) 2017 Théo FIDRY <theo.fidry@gmail.com>,
+ *                    Pádraic Brady <padraic.brady@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'meta' => [
+        'title' => 'Enum declaration',
+        // Default values. If not specified will be the one used
+        'prefix' => 'Humbug',
+
+        'expose-global-constants' => false,
+        'expose-global-classes' => false,
+        'expose-global-functions' => false,
+        'expose-namespaces' => [],
+        'expose-constants' => [],
+        'expose-classes' => [],
+        'expose-functions' => [],
+
+        'exclude-namespaces' => [],
+        'exclude-constants' => [],
+        'exclude-classes' => [],
+        'exclude-functions' => [],
+
+        'expected-recorded-classes' => [],
+        'expected-recorded-functions' => [],
+    ],
+
+    'nominal' => <<<'PHP'
+    <?php
+    
+    enum Foo {}
+    
+    class Enum {}
+    ----
+    <?php
+    
+    namespace Humbug;
+    
+    enum Foo
+    {
+    }
+    class Enum
+    {
+    }
+
+    PHP,
+];
