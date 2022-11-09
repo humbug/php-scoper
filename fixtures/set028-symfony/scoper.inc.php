79d6972b1030f00d2c6a25daa4acf21d7457ac85
Théo FIDRY <theo.fidry@gmail.com>
2022-11-10 00:07:08 +0100

    Simplify config

--- fixtures/set028-symfony/scoper.inc.php
+++ fixtures/set028-symfony/scoper.inc.php
@@ -14,41 +14,9 @@ declare(strict_types=1);
 
 use Isolated\Symfony\Component\Finder\Finder;
 
-$polyfillsBootstraps = array_map(
-    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
-    iterator_to_array(
-        Finder::create()
-            ->files()
-            ->in(__DIR__.'/vendor/symfony/polyfill-*')
-            ->name('bootstrap*.php'),
-        false,
-    ),
-);
-
-$polyfillsStubs = array_map(
-    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
-    iterator_to_array(
-        Finder::create()
-            ->files()
-            ->in(__DIR__.'/vendor/symfony/polyfill-*/Resources/stubs')
-            ->name('*.php'),
-        false,
-    ),
-);
-
 return [
-    'exclude-namespaces' => [
-//        'Symfony\Polyfill',
-    ],
     'exclude-constants' => [
         // Symfony global constants
         '/^SYMFONY\_[\p{L}_]+$/',
     ],
-    'exclude-files' => [
-//        ...$polyfillsBootstraps,
-//        ...$polyfillsStubs,
-    ],
-    'expose-classes' => [
-//        'Composer\InstalledVersions',
-    ]
 ];
