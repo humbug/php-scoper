4ddd00d6664dfbf22343b370e7403f7137e045f9
Théo FIDRY <theo.fidry@gmail.com>
2022-11-12 00:28:12 +0100

    Enable back Laravel test

--- fixtures/set027-laravel/scoper.inc.php
+++ fixtures/set027-laravel/scoper.inc.php
@@ -2,6 +2,33 @@
 
 declare(strict_types=1);
 
+use Isolated\Symfony\Component\Finder\Finder;
+
+$consoleViewFiles = array_map(
+    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
+    iterator_to_array(
+        Finder::create()
+            ->in('vendor/laravel/framework/src/Illuminate/Console/resources/views')
+            ->files(),
+        false,
+    ),
+);
+
 return [
-    'expose-global-functions' => true,
+    'exclude-files' => [
+        ...$consoleViewFiles,
+    ],
+    'patchers' => [
+        static function (string $filePath, string $prefix, string $contents): string {
+            if (!str_ends_with($filePath, 'vendor/laravel/framework/src/Illuminate/Console/View/Components/Factory.php')) {
+                return $contents;
+            }
+
+            return str_replace(
+                '$component = \'\\\\Illuminate\\\\Console\\\\View\\\\Components\\\\\' . \ucfirst($method);',
+                '$component = \'\\\\'.$prefix.'\\\\Illuminate\\\\Console\\\\View\\\\Components\\\\\' . \ucfirst($method);',
+                $contents,
+            );
+        },
+    ],
 ];
