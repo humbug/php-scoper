3be1036c8d9c0b03d3f3de07cd76fcb3477d0612
Théo FIDRY <theo.fidry@gmail.com>
2022-11-11 22:24:24 +0100

    Fix CS

--- scoper.inc.php
+++ scoper.inc.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-use Isolated\Symfony\Component\Finder\Finder;
-
 $jetBrainStubs = (static function (): array {
     $files = [];
 
