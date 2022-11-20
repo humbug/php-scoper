f7bd92f2459f1d9a643313f6d324476b0e23e087
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2022-11-20 14:00:42 +0100

    Fix the autoloading of Composer files (#774)
    Related to #298

--- fixtures/set035-composer-files-autoload/test.php
+++ fixtures/set035-composer-files-autoload/test.php
@@ -2,7 +2,7 @@
 
 $output = file_get_contents(__DIR__.'/output');
 
-$functionAutoloadFailed = 0 === preg_match(
+$functionAutoloadFailed = 1 === preg_match(
     '#PHP Fatal error:  Uncaught Error: Call to undefined function GuzzleHttp\\describe_type\(\)#',
     $output,
 );
