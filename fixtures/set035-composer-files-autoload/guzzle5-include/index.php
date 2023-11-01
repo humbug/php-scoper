49bd7faf8f86dd73a27e29d30a7e8140efa0b04c
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2023-11-01 11:47:22 +0100

    test: Improve the test 035 (#867)
    - Rework the test script to be clearer.
    - Add the versions of the package detected.
    - Pin the versions to avoid having the output changing.

--- fixtures/set035-composer-files-autoload/guzzle5-include/index.php
+++ fixtures/set035-composer-files-autoload/guzzle5-include/index.php
@@ -1,9 +1,14 @@
 <?php declare(strict_types=1);
 
-require file_exists(__DIR__.'/vendor/scoper-autoload.php')
-    ? __DIR__.'/vendor/scoper-autoload.php'
-    : __DIR__.'/vendor/autoload.php';
+use Composer\InstalledVersions;
+
+require file_exists(__DIR__ . '/vendor/scoper-autoload.php')
+    ? __DIR__ . '/vendor/scoper-autoload.php'
+    : __DIR__ . '/vendor/autoload.php';
 
 // Nothing to do: this file is purely here to autoload the scoped code.
 // This should trigger the auto-loading of guzzlehttp/guzzle/src/functions_include.php
 // which declares describe_type() – which is scoped.
+
+echo InstalledVersions::getVersion('guzzlehttp/guzzle') . PHP_EOL;
+echo "Guzzle5 loaded." . PHP_EOL;
--- fixtures/set035-composer-files-autoload/test.php
+++ fixtures/set035-composer-files-autoload/test.php
@@ -1,16 +1,37 @@
 <?php declare(strict_types=1);
 
-$output = file_get_contents(__DIR__.'/output');
-
-$functionAutoloadFailed = 1 === preg_match(
-    '#PHP Fatal error:  Uncaught Error: Call to undefined function GuzzleHttp\\describe_type\(\)#',
-    $output,
+$output = implode(
+    "\n",
+    array_filter(
+        array_map(
+            trim(...),
+            explode(PHP_EOL, file_get_contents(__DIR__ . '/output')),
+        ),
+        static fn(string $line) => '' !== $line,
+    ),
 );
-$expectedResult = false;
 
-if ($functionAutoloadFailed) {
-    echo $output;
-    echo PHP_EOL;
+$expectedOutput = <<<'EOF'
+Autoload Scoped code.
+5.3.4.0
+Guzzle5 loaded.
+Autoload code.
+6.5.8.0
+Done.
+EOF;
+
+if ($output !== $expectedOutput) {
+    echo 'FAILED!' . PHP_EOL;
+    echo 'Expected:' . PHP_EOL;
+    echo '–––––––' . PHP_EOL;
+    echo $expectedOutput . PHP_EOL;
+    echo '–––––––' . PHP_EOL;
+    echo 'Actual:' . PHP_EOL;
+    echo '–––––––' . PHP_EOL;
+    echo $output . PHP_EOL;
+    echo '–––––––' . PHP_EOL;
+
+    exit(1);
 }
 
-exit($functionAutoloadFailed === $expectedResult ? 0 : 1);
+exit(0);
