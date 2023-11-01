3850b42f854457fa994aff8a98fb51c61cf9d91d
Théo FIDRY <theo.fidry@gmail.com>
2023-11-01 09:52:24 +0100

    more details

--- fixtures/set035-composer-files-autoload/test.php
+++ fixtures/set035-composer-files-autoload/test.php
@@ -1,6 +1,15 @@
 <?php declare(strict_types=1);
 
-$output = file_get_contents(__DIR__.'/output');
+$output = implode(
+    "\n",
+    array_filter(
+        array_map(
+            trim(...),
+            explode(PHP_EOL, file_get_contents(__DIR__.'/output')),
+        ),
+        static fn (string $line) => '' !== $line,
+    ),
+);
 
 $expectedOutput = <<<'EOF'
 Autoload Scoped code.
@@ -8,14 +17,18 @@ Autoload Scoped code.
 Guzzle5 loaded.Autoload code.
 6.5.8.0
 Done.
-
 EOF;
 
 if ($output !== $expectedOutput) {
     echo 'FAILED!' . PHP_EOL;
+    echo 'Expected:' . PHP_EOL;
     echo '–––––––' . PHP_EOL;
-    echo $output;
-    echo PHP_EOL;
+    echo $expectedOutput. PHP_EOL;
+    echo '–––––––' . PHP_EOL;
+    echo 'Actual:' . PHP_EOL;
+    echo '–––––––' . PHP_EOL;
+    echo $output. PHP_EOL;
+    echo '–––––––' . PHP_EOL;
 
     exit(1);
 }
