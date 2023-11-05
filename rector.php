50843fac284b7e540281b94eaf9ecbf898525a9b
Viktor Szépe <viktor@szepe.net>
2023-11-05 19:51:42 +0100

    build: Remove non-existent ignored files (#900)

--- rector.php
+++ rector.php
@@ -7,10 +7,7 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/bin/check-composer-root-version.php',
-        __DIR__ . '/bin/dump-composer-root-version.php',
         __DIR__ . '/bin/php-scoper',
-        __DIR__ . '/bin/root-version.php',
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
