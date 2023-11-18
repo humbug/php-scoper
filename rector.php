1b03bbab183a24d16d6877a4243653adf9f2a3b4
Théo FIDRY <theo.fidry@gmail.com>
2023-11-18 09:55:18 +0100

    update composer and the rector config

--- rector.php
+++ rector.php
@@ -21,9 +21,27 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_82,
     ]);
 
     $rectorConfig->skip([
-        'NullToStrictStringFuncCallArgRector'
+        __DIR__.'/src/PhpParser/TraverserFactory.php',
+        __DIR__.'/tests/PhpParser/UseStmtNameTest.php',
+        __DIR__.'/src/PhpParser/NodeVisitor/AttributeAppender/ParentNodeAppender.php',
+
+        \Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
+        \Rector\Php73\Rector\String_\SensitiveHereNowDocRector::class,
+        \Rector\Php81\Rector\ClassMethod\NewInInitializerRector::class => [
+            __DIR__.'/src/Configuration/Configuration.php',
+        ],
+        \Rector\Php81\Rector\Property\ReadOnlyPropertyRector::class => [
+            __DIR__.'/src/Configuration/Configuration.php',
+        ],
+        \Rector\Php55\Rector\String_\StringClassNameToClassConstantRector::class => [
+            __DIR__.'/tests/Symbol/NamespaceRegistryTest.php',
+            __DIR__.'/tests/Symbol/Reflector/UserSymbolsReflectorTest.php',
+            __DIR__.'/tests/Symbol/SymbolRegistryTest.php',
+            __DIR__.'/tests/Symbol/SymbolsRegistryTest.php',
+        ],
     ]);
 };
