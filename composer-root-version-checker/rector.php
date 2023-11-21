79b08358130e132ba54d7e38adfecaeb3865a5d7
Théo FIDRY <theo.fidry@gmail.com>
2023-11-21 21:56:31 +0100

    fix

--- composer-root-version-checker/rector.php
+++ composer-root-version-checker/rector.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 /*
  * This file is part of the humbug/php-scoper package.
  *
@@ -11,6 +10,7 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector;
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
@@ -36,6 +36,6 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->rules([
-        \Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector::class,
+        CoversAnnotationWithValueToAttributeRector::class,
     ]);
 };
