940c3ae865b30ad048a2663dae474fd8096526f5
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2023-11-21 21:22:23 +0100

    build: Simplify rector configuration (#920)

--- composer-root-version-checker/rector.php
+++ composer-root-version-checker/rector.php
@@ -31,9 +31,7 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames();
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
         LevelSetList::UP_TO_PHP_82,
-
         PHPUnitSetList::PHPUNIT_90,
     ]);
 };
