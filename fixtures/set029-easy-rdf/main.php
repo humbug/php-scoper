ccf574db379dc4f1ccc6e8c3d56a2e779de4cfa3
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2024-05-13 23:47:51 +0200

    test: standardize the autoload usage (#1031)

--- fixtures/set029-easy-rdf/main.php
+++ fixtures/set029-easy-rdf/main.php
@@ -2,13 +2,9 @@
 
 declare(strict_types=1);
 
-$autoload = __DIR__.'/vendor/scoper-autoload.php';
-
-if (false === file_exists($autoload)) {
-    $autoload = __DIR__.'/vendor/autoload.php';
-}
-
-require_once $autoload;
+require file_exists(__DIR__.'/vendor/scoper-autoload.php')
+    ? __DIR__.'/vendor/scoper-autoload.php'
+    : __DIR__.'/vendor/autoload.php';
 
 $foaf = new EasyRdf\Graph('http://njh.me/foaf.rdf');
 $count = $foaf->load();
